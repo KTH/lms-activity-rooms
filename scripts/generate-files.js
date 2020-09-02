5c1af9bd1128106cae635a6b3477dca1e224f933
Carlos Saito <carlos@saito.style>
2020-09-02 17:29:20 +0200

    Add logic for generating examiners.csv

--- scripts/generate-files.js
+++ scripts/generate-files.js
@@ -4,8 +4,8 @@ const { writeActivities } = require('../lib')
 
 async function start () {
   log.info('Hello world')
-  const startDate = new Date('2020-10-01T00:00:00Z')
-  const endDate = new Date('2020-10-08T23:59:59Z')
+  const startDate = new Date(`${process.env.START_DATE}T00:00:00Z`)
+  const endDate = new Date(`${process.env.END_DATE}T23:59:59Z`)
   await writeActivities(startDate, endDate, '/tmp/activities/')
 }
 
