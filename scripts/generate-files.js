694a1c9427dc077a0fdce37091992736948defab
Carlos Saito <carlos@saito.style>
2020-09-02 16:23:17 +0200

    Fetch activities from akt-api

--- scripts/generate-files.js
+++ scripts/generate-files.js
@@ -6,7 +6,7 @@ async function start () {
   log.info('Hello world')
   const startDate = new Date('2020-10-01T00:00:00Z')
   const endDate = new Date('2020-10-08T23:59:59Z')
-  await syncActivities(startDate, endDate)
+  await writeActivities(startDate, endDate, '/tmp/activities/')
 }
 
 start()
