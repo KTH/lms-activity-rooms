a6594e221c4302b88e2632d6c1c53eab0bfcccd8
Carlos Saito <carlos@saito.style>
2020-09-02 16:11:29 +0200

    Loop through dates

--- scripts/generate-files.js
+++ scripts/generate-files.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
+const log = require('skog')
+const { syncActivities } = require('../lib')
 
 async function start () {
-  console.log('Hello world')
+  log.info('Hello world')
+  const startDate = new Date('2020-10-01T00:00:00Z')
+  const endDate = new Date('2020-10-08T23:59:59Z')
+  await syncActivities(startDate, endDate)
 }
 
 start()
