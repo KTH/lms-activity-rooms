ffac43bf8aad82c2021361084f56856b55e8a788
Carlos Saito <carlos@saito.style>
2020-09-28 12:32:29 +0200

    Use YYYY-MM-DD format

--- scripts/generate-files.js
+++ scripts/generate-files.js
@@ -12,10 +12,10 @@ async function start () {
 
 async function start2 () {
   log.info('Hello world')
-  const startDate = new Date('August 1, 2020 23:15:30')
-  const endDate = new Date('August 30, 2020 23:15:30')
+  const startDate = new Date('2020-10-01T23:15:30')
+  const endDate = new Date('2020-10-05T23:15:30')
 
   await syncActivities(startDate, endDate)
 }
 
-start()
+start2()
