bb11d6683511ad067ed2c7e222cd00c50ec586db
Carlos Saito <carlos@saito.style>
2020-11-20 12:37:30 +0100

    Do not check duration in monitor page

--- server/index.js
+++ server/index.js
@@ -3,34 +3,16 @@ const app = express()
 const prefix = process.env.PROXY_PATH || ''
 const cron = require('../cron')
 const canvas = require('../lib/canvas')
-const log = require('skog')
-
-function checkDuration () {
-  if (!cron.isRunning()) {
-    return { ok: true, msg: 'OK' }
-  }
-
-  const limit = 24 * 3600 * 1000
-  const duration = new Date() - cron.getStartTime()
-
-  if (duration > limit) {
-    return { ok: false, msg: `ERROR. Current sync is running for more than one day. Started: ${cron.getStartTime()}` }
-  } else {
-    return { ok: true, msg: 'OK. Current sync is running for less than one day.' }
-  }
-}
 
 app.get(prefix + '/_monitor', async (req, res) => {
   const canvasStatus = await canvas.test()
-  const durationStatus = checkDuration()
 
   res.setHeader('Content-Type', 'text/plain')
   res.send(
     [
-      `APPLICATION_STATUS: ${canvasStatus && durationStatus.ok ? 'OK' : 'ERROR'}`,
+      `APPLICATION_STATUS: ${canvasStatus ? 'OK' : 'ERROR'}`,
       '',
       `- CANVAS TOKEN: ${canvasStatus ? 'OK' : 'ERROR. Token not valid'}`,
-      `- DURATION: ${durationStatus.msg}`
     ].join('\n') + '\n'
   )
 })
