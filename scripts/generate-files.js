2c42044cedc26c6637d21facb3f1ce9162647302
Johan Öhman Saldes <johan.saldes@gmail.com>
2021-01-28 16:32:07 +0100

    start2 is unused

--- scripts/generate-files.js
+++ scripts/generate-files.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const log = require('skog')
-const { writeActivities, syncActivities } = require('../lib')
+const { writeActivities } = require('../lib')
 
 async function start () {
   log.info('Hello world')
@@ -8,14 +8,6 @@ async function start () {
   const endDate = new Date('August 30, 2020 23:15:30')
 
   await writeActivities(startDate, endDate, '/tmp/activities/')
-}
-
-async function start2 () {
-  log.info('Hello world')
-  const startDate = new Date('August 1, 2020 23:15:30')
-  const endDate = new Date('August 30, 2020 23:15:30')
-
-  await syncActivities(startDate, endDate)
 }
 
 start()
