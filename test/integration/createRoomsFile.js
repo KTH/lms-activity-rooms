c33c33c0fd728aa4553d885fe70b2fa266434f75
Johan Öhman Saldes <johan.saldes@gmail.com>
2021-02-01 23:23:46 +0100

    added test with mock data

--- test/integration/createRoomsFile.js
+++ test/integration/createRoomsFile.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
 const test = require('ava')
-const { writeActivities } = require('../../lib')
+const nock = require('nock')
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
+const { writeActivities, getActivities } = require('../../lib')
+
+const mockFileName = path.resolve('test', 'integration', 'files', 'answer.json');
+const mockData = fs.readFileSync(mockFileName, { encoding: 'utf-8' });
 
 const dir = fs.promises.mkdtemp(path.join(os.tmpdir(), 'exams-'))
 const start = new Date('August 1, 2020 23:15:30')
 const end = new Date('August 15, 2020 23:15:30')
 
-test.before(async () => {
+test.beforeEach(async () => {
+  nock(`${process.env.AKTIVITETSTILLFALLEN_API_URL}/aktivitetstillfallen`)
+  .get('/students')
+  .query(true)
+  .reply(200, JSON.parse(mockData)).persist()
   await writeActivities(start, end, await dir)
 })
 
@@ -31,6 +39,5 @@ test(`Generating lms-activity-rooms.examiners.csv ${start} - ${end}`, async t =>
 test(`lms-activity-rooms.students.csv is not empty`, async t => {
   const fileName = path.resolve(await dir, 'lms-activity-rooms.students.csv')
   const content = fs.readFileSync(fileName, { encoding: 'utf-8' })
-  // Just confirm that this isn't empty. Could contain sensitive information about students.
-  t.assert(content.length)
+  t.snapshot(content)
 })
