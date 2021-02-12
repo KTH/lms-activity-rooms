cd02570c1bd46199946195e35fa7591565120054
Johan Öhman Saldes <johan.saldes@gmail.com>
2021-02-12 14:07:03 +0100

    impelemented snapshot and deep equal on cvs

--- test/integration/createRoomsFile.js
+++ test/integration/createRoomsFile.js
@@ -6,39 +6,121 @@ const path = require('path')
 const os = require('os')
 const { writeActivities } = require('../../lib')
 
-const mockFileName = path.resolve('test', 'integration', 'files', 'answer.json')
-const mockData = fs.readFileSync(mockFileName, { encoding: 'utf-8' })
+/**
+ * Testing that, given that API responses are as defined this function
+ * should successfully write certain files to a certain directory
+ * with a predefined strucuture in .cvs format.
+ */
 
-const dir = fs.promises.mkdtemp(path.join(os.tmpdir(), 'exams-'))
-const start = new Date('August 1, 2020 23:15:30')
-const end = new Date('August 15, 2020 23:15:30')
-
-test.beforeEach(async () => {
+test.before(async t => {
+  const dir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'exams-'))
+  /**
+   * Context is a good practice for writing DRY code
+   */
+  t.context = {
+    mock: fs.readFileSync(
+      path.resolve('test', 'integration', 'files', 'mock.json'),
+      { encoding: 'utf-8' }
+    ),
+    files: {
+      courses: path.resolve(dir, 'lms-activity-rooms.courses.csv'),
+      sections: path.resolve(dir, 'lms-activity-rooms.sections.csv'),
+      examiners: path.resolve(dir, 'lms-activity-rooms.examiners.csv'),
+      students: path.resolve(dir, 'lms-activity-rooms.students.csv')
+    },
+    getHeader: fileName => {
+      return fs
+        .readFileSync(fileName, { encoding: 'utf-8' })
+        .split('\n')[0]
+        .split(',')
+    }
+  }
+  /**
+   * This will intercept any fetch requests made and reply with our mock
+   */
   nock(`${process.env.AKTIVITETSTILLFALLEN_API_URL}/aktivitetstillfallen`)
     .get('/students')
     .query(true)
-    .reply(200, JSON.parse(mockData))
+    .reply(200, JSON.parse(t.context.mock))
     .persist()
-  await writeActivities(start, end, await dir)
+  /**
+   * Running the integration test
+   */
+  await writeActivities(
+    new Date('August 1, 2020 23:15:30'),
+    new Date('August 15, 2020 23:15:30'),
+    dir
+  )
 })
 
-test(`Generating lms-activity-rooms.courses.csv ${start} - ${end}`, async t => {
-  const fileName = path.resolve(await dir, 'lms-activity-rooms.courses.csv')
-  const content = fs.readFileSync(fileName, { encoding: 'utf-8' })
-  t.assert(content.length)
+/**
+ * Each test reads file that should exists and compares with snapshot
+ * given that they match and file exists the test passes.
+ */
+test('Generating lms-activity-rooms.courses.csv', async t => {
+  const content = fs.readFileSync(t.context.files.courses, {
+    encoding: 'utf-8'
+  })
+  t.snapshot(content)
 })
-test(`Generating lms-activity-rooms.sections.csv ${start} - ${end}`, async t => {
-  const fileName = path.resolve(await dir, 'lms-activity-rooms.sections.csv')
-  const content = fs.readFileSync(fileName, { encoding: 'utf-8' })
-  t.assert(content.length)
+test('Generating lms-activity-rooms.sections.csv', async t => {
+  const content = fs.readFileSync(t.context.files.sections, {
+    encoding: 'utf-8'
+  })
+  t.snapshot(content)
 })
-test(`Generating lms-activity-rooms.examiners.csv ${start} - ${end}`, async t => {
-  const fileName = path.resolve(await dir, 'lms-activity-rooms.examiners.csv')
-  const content = fs.readFileSync(fileName, { encoding: 'utf-8' })
-  t.assert(content.length)
+test('Generating lms-activity-rooms.examiners.csv', async t => {
+  const content = fs.readFileSync(t.context.files.examiners, {
+    encoding: 'utf-8'
+  })
+  t.snapshot(content)
 })
-test(`lms-activity-rooms.students.csv is not empty`, async t => {
-  const fileName = path.resolve(await dir, 'lms-activity-rooms.students.csv')
-  const content = fs.readFileSync(fileName, { encoding: 'utf-8' })
-  t.assert(content.length)
+test('Generating lms-activity-rooms.students.csv', async t => {
+  const content = fs.readFileSync(t.context.files.students, {
+    encoding: 'utf-8'
+  })
+  t.snapshot(content)
+})
+
+/**
+ * Testing the structural integrity of our headers in the cvs files.
+ */
+test('Structural integrity of courses files', t => {
+  const { courses } = t.context.files
+  t.deepEqual(t.context.getHeader(courses), [
+    'course_id',
+    'short_name',
+    'long_name',
+    'account_id',
+    'status',
+    'blueprint_course_id'
+  ])
+})
+test('Structural integrity of examiners files', t => {
+  const { examiners } = t.context.files
+  t.deepEqual(t.context.getHeader(examiners), [
+    'section_id',
+    'user_id',
+    'role_id',
+    'status'
+  ])
+})
+test('Structural integrity of sections files', t => {
+  const { sections } = t.context.files
+  t.deepEqual(t.context.getHeader(sections), [
+    'course_id',
+    'section_id',
+    'name',
+    'status'
+  ])
+})
+test('Structural integrity of students files', t => {
+  const { students } = t.context.files
+  t.deepEqual(t.context.getHeader(students), [
+    'section_id',
+    'user_id',
+    'role_id',
+    'status',
+    'limit_section_privileges'
+  ])
 })
